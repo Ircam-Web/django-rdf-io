5854fb5e57e92103595c95914c0adb092059dd89
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2019-11-05 12:20:41 +0100

    fix app init

--- rdf_io/__init__.py
+++ rdf_io/__init__.py
@@ -1,6 +1,11 @@
+from __future__ import unicode_literals
+
 try:
    from django.core import signals
 except:
     import signals
 
+
 __version__ = (0, 1)
+
+default_app_config = 'rdf_io.apps.RDFIOConfig'
